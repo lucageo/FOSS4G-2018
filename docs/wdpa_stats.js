f5047058215fc19e69587479aced7f30e8a2b98d
luca <luca.battistella.geo@gmail.com>
2018-08-16 07:55:57 +0000

    Update wdpa_stats.js

--- docs/wdpa_stats.js
+++ docs/wdpa_stats.js
@@ -1,32 +1,32 @@
 
 //Initialise map
 var pixel_ratio = parseInt(window.devicePixelRatio) || 1;
+
 var max_zoom = 16;
 var tile_size = 512;
+
 var map = L.map('map', {
 }).setView([-7, 38], 6);
 
 
-// basemaps
-var WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
- attribution: ''
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+ attribution: 'October 2017 version of the World Database on Protected Areas (WDPA)'
 });
-var light  =  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-subdomains: 'abcd',
-maxZoom: 19
+var light  = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+  subdomains: 'abcd',
+  opacity: 1,
+  attribution: 'October 2017 version of the World Database on Protected Areas (WDPA)',
+  maxZoom: 19
+}).addTo(map);
+var topLayer =  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_only_labels/{z}/{x}/{y}.png', {
+  subdomains: 'abcd',
+  opacity: 1,
+  maxZoom: 19
 }).addTo(map);
 
 
-// wdpa layer
-var url = 'https://lrm-maps.jrc.ec.europa.eu/geoserver/dopa_explorer_2/wms';
-var wdpa=L.tileLayer.wms(url, {
-layers: 'dopa_explorer_2:wdpa_foss4g',
-transparent: true,
-format: 'image/png',
-opacity:'1',
-zIndex: 33
-}).addTo(map);
+
 
 
 // on click function
@@ -34,80 +34,94 @@ map.on('click', function(e) {
 	 if (map.hasLayer(wdpa)) {
 		var latlng= e.latlng;
 		var url = getFeatureInfoUrl(
-				map,
-				wdpa,
-				e.latlng,
-					{
-					'info_format': 'text/javascript',  //it allows us to get a jsonp
-					'propertyName': ' wdpa_name,wdpaid',
-					'query_layers': 'dopa_explorer_2:wdpa_foss4g',
-					'format_options':'callback:getJson'
-					}
-				);
+										map,
+										wdpa,
+										e.latlng,
+										{
+												'info_format': 'text/javascript',  //it allows us to get a jsonp
+												'propertyName': ' wdpa_name,wdpaid,rep_area',
+												'query_layers': 'dopa_explorer_2:wdpa_foss4g',
+												'format_options':'callback:getJson'
+										}
+								);
 			 $.ajax({
-				 jsonp: false,
-				 url: url,
-				 dataType: 'jsonp',
-				 jsonpCallback: 'getJson',
-				 success: handleJson_featureRequest
-			 });
-				function handleJson_featureRequest(data)
-				{
-				if (typeof data.features[0]!=='undefined')
-				{
-				var prop=data.features[0].properties;
-				var filter="wdpaid='"+prop['wdpaid']+"'";
-				wdpa_hi.setParams({CQL_FILTER:filter});
-				hi_highcharts_wdpa(prop,latlng);
-				}
-				else {}
-				}
-				}
-				else {
-				}
+							 jsonp: false,
+							 url: url,
+							 dataType: 'jsonp',
+							 jsonpCallback: 'getJson',
+							 success: handleJson_featureRequest
+						 });
+					function handleJson_featureRequest(data)
+					{
+						 if (typeof data.features[0]!=='undefined')
+								 {
+										var prop=data.features[0].properties;
+										var filter="wdpaid='"+prop['wdpaid']+"'";
+										wdpa_hi.setParams({CQL_FILTER:filter});
+										// m_eco_hi.setParams({CQL_FILTER:filter});
+										hi_highcharts_wdpa(prop,latlng);
+							}
+							else {}
+						}
+				 }
+				 else {
+			 }
 });
 
-
-// get feature info function
-function getFeatureInfoUrl(map, layer, latlng, params) {
-
-var point = map.latLngToContainerPoint(latlng, map.getZoom()),
-    size = map.getSize(),
-    bounds = map.getBounds(),
-    sw = bounds.getSouthWest(),
-    ne = bounds.getNorthEast();
-var defaultParams = {
-    request: 'GetFeatureInfo',
-    service: 'WMS',
-    srs: 'EPSG:4326',
-    styles: '',
-    version: layer._wmsVersion,
-    format: layer.options.format,
-    bbox: bounds.toBBoxString(),
-    height: size.y,
-    width: size.x,
-    layers: layer.options.layers,
-    info_format: 'text/javascript'
-};
-params = L.Util.extend(defaultParams, params || {});
-params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
-params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
-return layer._url + L.Util.getParamString(params, layer._url, true);
-}
-
-// WDPA HIGLIGHTED LAYER
+// wdpa layer
 var url = 'https://lrm-maps.jrc.ec.europa.eu/geoserver/dopa_explorer_2/wms';
-var wdpa_hi=L.tileLayer.wms(url, {
-layers: 'dopa_explorer_2:wdpa_foss4g',
-transparent: true,
-format: 'image/png',
-opacity:'1',
-styles: 'polygon',
-zIndex: 44
-}).addTo(map);
+var wdpa=L.tileLayer.wms(url, {
+		layers: 'dopa_explorer_2:wdpa_foss4g',
+		transparent: true,
+		format: 'image/png',
+		//styles: 'terrestrial_ecoregion_protection_2018',
+		opacity:'1',
+		zIndex: 33
+	}).addTo(map);
+
+
+	// get feature info function
+	function getFeatureInfoUrl(map, layer, latlng, params) {
+
+	    var point = map.latLngToContainerPoint(latlng, map.getZoom()),
+	        size = map.getSize(),
+	        bounds = map.getBounds(),
+	        sw = bounds.getSouthWest(),
+	        ne = bounds.getNorthEast();
+
+	    var defaultParams = {
+	        request: 'GetFeatureInfo',
+	        service: 'WMS',
+	        srs: 'EPSG:4326',
+	        styles: '',
+	        version: layer._wmsVersion,
+	        format: layer.options.format,
+	        bbox: bounds.toBBoxString(),
+	        height: size.y,
+	        width: size.x,
+	        layers: layer.options.layers,
+	        info_format: 'text/javascript'
+	    };
+
+	    params = L.Util.extend(defaultParams, params || {});
+	    params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+	    params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+	    return layer._url + L.Util.getParamString(params, layer._url, true);
+	}
+
+// wdpa HIGLIGHTED
+	var url = 'https://lrm-maps.jrc.ec.europa.eu/geoserver/dopa_explorer_2/wms';
+	var wdpa_hi=L.tileLayer.wms(url, {
+		  layers: 'dopa_explorer_2:wdpa_foss4g',
+			transparent: true,
+			format: 'image/png',
+			opacity:'1',
+			styles: 'polygon',
+			zIndex: 44
+	 }).addTo(map);
 wdpa_hi.setParams({CQL_FILTER:"wdpaid LIKE ''"});
 
-// charts function
+	// charts function
 function hi_highcharts_wdpa(info,latlng){
  var wdpa_name=info['wdpa_name'];
  var wdpaid=info['wdpaid'];
@@ -234,17 +248,19 @@ var url_wdpaid_lcc = 'http://localhost:8888/rest.py?type=fun&schema=public&obj=g
 
 }
 
-// HIDE CHARTS WHEN CLOSING POPUP
-map.on('popupclose', function (){
+map.on('popupclose', function (){ //map is the name of map you gave to your leaflet map
 	$( "#wdpa_plot_1995" ).hide();
 	$( "#wdpa_plot_2015" ).hide();
-	$( "#wdpa_plot_1995_title" ).hide();
-	$( "#wdpa_plot_2015_title" ).hide();
 });
 
+
+
+
+
+
 //Available Layers
-var baseMaps = {"White" : light, "WorldImagery":WorldImagery};
+var baseMaps = {"White" : light, "Esri_WorldImagery":Esri_WorldImagery};
 var overlayMaps = {'wdpa': wdpa};
 
 //Add Layer Control
-layerControl = L.control.layers(baseMaps, overlayMaps, null,  {position: 'topleft'}).addTo(map);
+layerControl = L.control.layers(baseMaps, overlayMaps, null,  {position: 'bottomleft'}).addTo(map);
